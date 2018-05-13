7780fa64ec6b836dcb9de8e40eb628764f0de1a5
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-05-12 22:25:06 -0300

    criacao de metodo nome existe, refatoracao de campo vazio, validacao de jogos excluidos

--- games/models.py
+++ games/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Game(models.Model):
-	created = models.DateTimeField(auto_now_add=True,blank=True)
-	name = models.CharField(max_length=200, blank=True, default='')
-	release_date = models.DateTimeField(blank=True)
-	game_category = models.CharField(max_length=200, blank=True, default='')
-	played = models.BooleanField(default=False,blank=True)
+	created = models.DateTimeField(auto_now_add=True,null=True)
+	name = models.CharField(max_length=200, null=True, default='')
+	release_date = models.DateTimeField(null=True)
+	game_category = models.CharField(max_length=200, null=True, default='')
+	played = models.BooleanField(default=False)
 
 	class Meta:
 		ordering = ('name',)
--- games/serializers.py
+++ games/serializers.py
@@ -1,9 +1,9 @@
+from datetime import date
 from django.db.models import Exists
 from rest_framework import serializers, request
 from rest_framework.decorators import api_view
 from rest_framework.fields import SlugField
 from rest_framework.validators import UniqueValidator
-
 from .models import Game
 
 
@@ -16,8 +16,8 @@ class GameSerializer(serializers.ModelSerializer):
 
     def field_valid(self, field):
         print(field, type(field))
-        if field is None:
-            raise serializers.ValidationError("Campo Vazio")
+        if field is None or field == "":
+            raise serializers.ValidationError("Campo Vazio, por favor preencha todos os campos")
 
         return field
 
@@ -26,10 +26,14 @@ class GameSerializer(serializers.ModelSerializer):
         game = Game.objects.filter(name=name).exists()
 
         if game:
-            raise serializers.ValidationError('Nome ja existente')
+            raise serializers.ValidationError('Nome ja existente, favor escolher outro nome de jogo')
         return self.field_valid(name)
 
     def validate_release_date(self,release_date):
+
+        if release_date  < date.today():
+            raise serializers.ValidationError('Jogo ainda não lançado. Exclusão não  permitida. ')
+
         return self.field_valid(release_date)
 
     def validate_game_category(self,game_category):
